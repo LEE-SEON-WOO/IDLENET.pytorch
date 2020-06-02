39d988e0dec8483610d361dff4cd95a8aa798d1d
Su Woong Hong <hsw2012s@gmail.com>
2020-06-02 12:30:51 +0900

    Find all file in data path and save result

--- infer.sh
+++ infer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 data="khnp"
 backbone="resnet101"
-model_name="/model-90000"
-f_name="518-PP04"
+model_name="/model/model-90000"
 blur_check=True
+data_path="/data/saewool1"
 
-python infer_result_save.py -s=${data} -b=${backbone} -c=${model_name}.pth /data/saewool1/color/${f_name}.jpg /result/${f_name}.jpg /data/saewool1/csv/${f_name}.csv /data/saewool1/annotation/${f_name}.xml /result/${f_name}.json ${blur_check}
+python infer_result_save.py -s=${data} -b=${backbone} -c=${model_name}.pth $data_path ${blur_check}
--- infer_result_save.py
+++ infer_result_save.py
@@ -11,7 +11,7 @@ from model import Model
 from roi.pooler import Pooler
 from config.eval_config import EvalConfig as Config
 import analyze_csv
-from hci import test
+from blur import blur
 
 def _infer(path_to_input_image: str, path_to_output_image: str, path_to_checkpoint: str, dataset_name: str, backbone_name: str, prob_thresh: float):
     dataset_class = DatasetBase.from_name(dataset_name)
@@ -67,48 +67,70 @@ if __name__ == '__main__':
         parser.add_argument('--pooler_mode', type=str, choices=Pooler.OPTIONS, help='default: {.value:s}'.format(Config.POOLER_MODE))
         parser.add_argument('--rpn_pre_nms_top_n', type=int, help='default: {:d}'.format(Config.RPN_PRE_NMS_TOP_N))
         parser.add_argument('--rpn_post_nms_top_n', type=int, help='default: {:d}'.format(Config.RPN_POST_NMS_TOP_N))
-        parser.add_argument('input', type=str, help='path to input image')
-        parser.add_argument('output', type=str, help='path to output result image')
-        parser.add_argument('csv_input', type=str, help='path to input csv')
-        parser.add_argument('xml_input', type=str, help='path to input xml')
-        parser.add_argument('json_result', type=str, help='path to json result')
+        # parser.add_argument('input', type=str, help='path to input image')
+        # parser.add_argument('output', type=str, help='path to output result image')
+        # parser.add_argument('csv_input', type=str, help='path to input csv')
+        # parser.add_argument('xml_input', type=str, help='path to input xml')
+        # parser.add_argument('json_result', type=str, help='path to json result')
+        parser.add_argument('data_path', type=str, help='data path')
         parser.add_argument('blur', type=bool, help='check blur image')
         args = parser.parse_args()
 
-        path_dir = os.path.dirname(os.path.realpath(__file__))
-        path_to_input_image = os.path.join(path_dir+args.input)
-        path_to_output_image = os.path.join(path_dir+args.output)
-        path_to_input_csv = os.path.join(path_dir+args.csv_input)
-        path_to_input_xml = os.path.join(path_dir+args.xml_input)
-        path_to_output_json = os.path.join(path_dir+args.json_result)
-        blur_check = args.blur
+        root_dir = os.path.dirname(os.path.realpath(__file__))
+        path_dir = os.path.join(root_dir + args.data_path)
 
-        if blur_check:
-            blur_confidence = test.run_example(path_to_input_image)
-            test.run_example(path_to_input_image)
+        image_path = os.path.join(path_dir+'/color')
+        image_path_list = [f for f in os.listdir(image_path) if f.endswith(".jpg")]
+        print(f"find {len(image_path_list)} images")
 
-        dataset_name = args.dataset
-        backbone_name = args.backbone
-        path_to_checkpoint = os.path.join(path_dir+args.checkpoint)
-        prob_thresh = args.probability_threshold
+        csv_path = os.path.join(path_dir+'/csv')
+        csv_path_list = [f for f in os.listdir(csv_path) if f.endswith(".csv")]
+        print(f"find {len(csv_path_list)} Csvs")
 
-        os.makedirs(os.path.join(os.path.curdir, os.path.dirname(path_to_output_image)), exist_ok=True)
+        xml_path = os.path.join(path_dir+'/annotation')
+        xml_path_list = [f for f in os.listdir(xml_path) if f.endswith(".xml")]
+        print(f"find {len(xml_path_list)} xmls")
+        
+        f_name_list = []
+        for image_path in image_path_list:
+            f_name = image_path[:-4]
+            f_name_list.append(f_name)
+            if not os.path.exists(os.path.join(root_dir+'/result/saewool1/'+f_name)):
+                os.makedirs(os.path.join(root_dir+'/result/saewool1/'+f_name))
 
-        Config.setup(image_min_side=args.image_min_side, 
-                            image_max_side=args.image_max_side,
-                            anchor_ratios=args.anchor_ratios, 
-                            anchor_sizes=args.anchor_sizes, 
-                            pooler_mode=args.pooler_mode,
-                            rpn_pre_nms_top_n=args.rpn_pre_nms_top_n, 
-                            rpn_post_nms_top_n=args.rpn_post_nms_top_n)
+        for f_name in f_name_list:
+            print(f"Proceeding {f_name_list.index(f_name)+1} file in {len(image_path_list)}")
+            path_to_input_image = os.path.join(path_dir+'/color/'+f_name+'.jpg')
+            path_to_input_csv = os.path.join(path_dir+'/csv/'+f_name+'.csv')
+            path_to_input_xml = os.path.join(path_dir+'/annotation/'+f_name+'.xml')
+            path_to_output_image = os.path.join(os.path.join(root_dir+'/result/saewool1/'+f_name))
+            path_to_output_json = os.path.join(os.path.join(root_dir+'/result/saewool1/'+f_name+'/'+f_name+'.json'))
+            blur_check = args.blur
 
-        print('Arguments:')
-        print(path_to_output_image)
-        for k, v in vars(args).items():
-            print(f'\t{k} = {v}')
-        print(Config.describe())
+            if blur_check:
+                blur_confidence = blur.run_example(path_to_input_image)
+                
+            dataset_name = args.dataset
+            backbone_name = args.backbone
+            path_to_checkpoint = os.path.join(root_dir+args.checkpoint)
+            prob_thresh = args.probability_threshold
 
-        _infer(path_to_input_image, path_to_output_image, path_to_checkpoint, dataset_name, backbone_name, prob_thresh)
-        analyze_csv.analyze(path_to_input_xml, path_to_input_csv, path_to_output_json, blur_confidence)
+            Config.setup(image_min_side=args.image_min_side, 
+                                image_max_side=args.image_max_side,
+                                anchor_ratios=args.anchor_ratios, 
+                                anchor_sizes=args.anchor_sizes, 
+                                pooler_mode=args.pooler_mode,
+                                rpn_pre_nms_top_n=args.rpn_pre_nms_top_n, 
+                                rpn_post_nms_top_n=args.rpn_post_nms_top_n)
+
+            print('Arguments:')
+            print(path_to_output_image)
+            for k, v in vars(args).items():
+                print(f'\t{k} = {v}')
+            print(Config.describe())
+
+            _infer(path_to_input_image, path_to_output_image, path_to_checkpoint, dataset_name, backbone_name, prob_thresh)
+            analyze_csv.analyze(path_to_input_xml, path_to_input_csv, path_to_output_json, blur_confidence)
 
     main()
+
